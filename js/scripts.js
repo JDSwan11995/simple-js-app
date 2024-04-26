2ab60af3f7aa8cfc545cb19832a5cb388d2ee6f1
JDSwan11995 <jdswan1995@gmail.com>
2024-04-26 18:53:07 -0500

    added temp content to index.html and Pokedex entry to script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello World');
-let favoriteFood=('Bolognese');
-document.write(favoriteFood);
+let pokemonList= [
+    {name: 'Cubone', type: 'Ground', number: '#104', disposition: 'Cuddly'},
+    {name: 'Slowpoke', type: ['Psychic', 'Water'], number: '#79', disposition:'Silly'},
+    {name: 'Mr. Mime', type: ['Psychic', 'Fairy'], number: '#122', disposition: 'Nightmare Fuel'},
+    {name: 'Articuno', type: ['Ice', 'Flying'], number: '#144', disposition: 'Godly'},
+    {name: 'Pecharunt', type: ['Poison', 'Ghost'], number: '#1025', disposition: 'Excessive'},
+    {name: 'Mimikyu', type: ['Ghost', 'Fairy'], number: '#778', disposition: 'Lonely'}
+]
