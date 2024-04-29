4fb2daca40d60897d9b9b8012a1198eaff6563d2
JDSwan11995 <jdswan1995@gmail.com>
2024-04-29 17:05:10 -0500

    Changed pokemonList readability and added comment for tall Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,52 @@
-let pokemonList= [
-    {name: 'Cubone', type: 'Ground', number: '#104', disposition: 'Cuddly'},
-    {name: 'Slowpoke', type: ['Psychic', 'Water'], number: '#79', disposition:'Silly'},
-    {name: 'Mr. Mime', type: ['Psychic', 'Fairy'], number: '#122', disposition: 'Nightmare Fuel'},
-    {name: 'Articuno', type: ['Ice', 'Flying'], number: '#144', disposition: 'Godly'},
-    {name: 'Pecharunt', type: ['Poison', 'Ghost'], number: '#1025', disposition: 'Excessive'},
-    {name: 'Mimikyu', type: ['Ghost', 'Fairy'], number: '#778', disposition: 'Lonely'}
-]
+let pokemonList = [
+    {
+        name: 'Cubone',
+        type: 'Ground',
+        height: 0.4,
+        number: 104,
+        disposition: 'Sad'
+    },
+    {
+        name: 'Slowpoke',
+        type: ['Psychic', 'Water'],
+        height: 1.9,
+        number: 79,
+        disposition: 'Silly'
+    },
+    {
+        name: 'Mr. Mime',
+        type: ['Psychic', 'Fairy'],
+        height: 1.8,
+        number: 122,
+        disposition: 'Nightmare Fuel'
+    },
+    {
+        name: 'Mimikyu',
+        type: ['Ghost', 'Fairy'],
+        height: 0.8,
+        number: 778,
+        disposition: 'Lonely'
+    },
+    {
+        name: 'Articuno',
+        type: ['Ice', 'Flying'],
+        height: 1.7,
+        number: 144,
+        disposition: 'Godly'
+    },
+    {
+        name: 'Pecharunt',
+        type: ['Poison', 'Ghost'],
+        height: 0.3,
+        number: 1025,
+        disposition: 'Excessive'
+    },
+];
+
+for (let i = 1; i < pokemonList.length; i++) {
+    if (pokemonList[i].height === 1.9) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "kg), -That's Tall! " + "<br>");
+    } else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "kg), " + "<br>")
+    }
+}
