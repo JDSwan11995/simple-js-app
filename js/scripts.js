e29003a4895a526ec8cbfe985051da96aa5b4a61
JDSwan11995 <jdswan1995@gmail.com>
2024-05-30 17:06:48 -0500

    Added IIFE and rewrote final for loop to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,67 @@
-let pokemonList = [
-    {
-        name: 'Cubone',
-        type: 'Ground',
-        height: 0.4,
-        number: 104,
-        disposition: 'Sad'
-    },
-    {
-        name: 'Slowpoke',
-        type: ['Psychic', 'Water'],
-        height: 1.9,
-        number: 79,
-        disposition: 'Silly'
-    },
-    {
-        name: 'Mr. Mime',
-        type: ['Psychic', 'Fairy'],
-        height: 1.8,
-        number: 122,
-        disposition: 'Nightmare Fuel'
-    },
-    {
-        name: 'Mimikyu',
-        type: ['Ghost', 'Fairy'],
-        height: 0.8,
-        number: 778,
-        disposition: 'Lonely'
-    },
-    {
-        name: 'Articuno',
-        type: ['Ice', 'Flying'],
-        height: 1.7,
-        number: 144,
-        disposition: 'Godly'
-    },
-    {
-        name: 'Pecharunt',
-        type: ['Poison', 'Ghost'],
-        height: 0.3,
-        number: 1025,
-        disposition: 'Excessive'
-    },
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Cubone',
+            type: 'Ground',
+            height: 0.4,
+            number: 104,
+            disposition: 'Sad'
+        },
+        {
+            name: 'Slowpoke',
+            type: ['Psychic', 'Water'],
+            height: 1.9,
+            number: 79,
+            disposition: 'Silly'
+        },
+        {
+            name: 'Mr. Mime',
+            type: ['Psychic', 'Fairy'],
+            height: 1.8,
+            number: 122,
+            disposition: 'Nightmare Fuel'
+        },
+        {
+            name: 'Mimikyu',
+            type: ['Ghost', 'Fairy'],
+            height: 0.8,
+            number: 778,
+            disposition: 'Lonely'
+        },
+        {
+            name: 'Articuno',
+            type: ['Ice', 'Flying'],
+            height: 1.7,
+            number: 144,
+            disposition: 'Godly'
+        },
+        {
+            name: 'Pecharunt',
+            type: ['Poison', 'Ghost'],
+            height: 0.3,
+            number: 1025,
+            disposition: 'Excessive'
+        },
+    ];
 
-function printArrayDetails(list){
-    for (let i = 0; i < list.length; i++) {
-        if (list[i].height >= 1.8) {
-            document.write("<p>" + list[i].name + " (height: " + list[i].height + "m) - That's Tall! " + "</p>") 
-            console.log(list[i].name + " height: " + list[i].height + "m");
-        } else {
-            document.write("<p>" + list[i].name + " (height: " + list[i].height + "m) " + "</p>")
-            console.log(list[i].name + " height: " + list[i].height + "m")
-        }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-};
 
+    function getAll() {
+        return pokemonList;
+    }
 
-printArrayDetails(pokemonList);
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
-
-// function divide(dividend, divisor){
-//     if(divisor === 0){
-//         return "You are trying to divide by zero, stop that, the universe is fragile enough."
-//     }else{
-//         let result = dividend / divisor;
-//         return result;
-//     }
-// };
-
-// let yourName = prompt('Can I have your name?');
-// console.log(yourName);
-
-// let isAccepted = confirm('Can I have your soul?');
-// console.log(isAccepted);
-
-// let howRude = alert('Whatcha got against fairies?')
+pokemonRepository.getAll().forEach(function(pokemon){
+    if (pokemon.height >= 1.8){
+        document.write("<p>" + pokemon.name + " (height: " + pokemon.height +"m) - Wow, that's tall! </p>")
+    } else if (pokemon.height){
+        document.write("<p>" + pokemon.name + " (height: " + pokemon.height +"m) </p>")
+    }
+});
